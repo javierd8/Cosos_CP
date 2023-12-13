90561c81ed8a9961a782531abad2e43163742007
javierd8 <48897035+javierd8@users.noreply.github.com>
2023-12-13 14:17:51 -0600

    Create thelastproblem.cpp

---
+++ ProblemSolving/Kattis/thelastproblem.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+void uwo();
+
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);    
+
+    uwo();
+
+    return 0;
+}
+
+void uwo(){
+    string s;
+    getline(cin,s);
+    cout<<"Thank you, "<<s<<", and farewell!";
+
+}
