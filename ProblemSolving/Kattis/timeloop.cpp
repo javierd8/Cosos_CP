65f95ff9cad2c0229e14410e06e1c02e600a036f
javierd8 <48897035+javierd8@users.noreply.github.com>
2023-12-13 14:18:49 -0600

    Create timeloop.cpp

---
+++ ProblemSolving/Kattis/timeloop.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+void uwo();
+
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);    
+
+    uwo();
+
+    return 0;
+}
+
+void uwo(){
+    int n;cin>>n;
+    int i=1;
+    while(i<=n){
+        cout<<i<<" Abracadabra"<<endl;
+        i++;
+    }
+
+}
