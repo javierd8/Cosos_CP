f1abbe24dc220fa6214bf479990f016c3b910de7
javierd8 <48897035+javierd8@users.noreply.github.com>
2023-12-13 14:17:30 -0600

    Create r2.cpp

---
+++ ProblemSolving/Kattis/r2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+void uwo();
+
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);    
+
+    uwo();
+
+    return 0;
+}
+
+void uwo(){
+    int r,s;cin>>r>>s;
+    cout<<2*s-r;
+}
