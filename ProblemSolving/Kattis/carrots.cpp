e17e453076a0db49d85be6289108149d8f7b1c28
javierd8 <48897035+javierd8@users.noreply.github.com>
2023-12-13 14:17:11 -0600

    Create carrots.cpp

---
+++ ProblemSolving/Kattis/carrots.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+void uwo();
+
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);    
+
+    uwo();
+
+    return 0;
+}
+
+void uwo(){
+    int n,p;cin>>n>>p;
+    while(n--){
+        string s;
+        getline(cin,s);
+    }
+    cout<<p;
+}
