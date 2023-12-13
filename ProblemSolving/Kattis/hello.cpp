d90611df284ff861096d0cc6fe238267481c5a49
javierd8 <48897035+javierd8@users.noreply.github.com>
2023-12-13 14:16:53 -0600

    Create hello.cpp

---
+++ ProblemSolving/Kattis/hello.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+void uwo();
+
+int main(){
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);    
+
+    uwo();
+
+    return 0;
+}
+
+void uwo(){
+    cout<<"Hello World!";
+}
